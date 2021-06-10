fe954037472d000221c35ea523f897653f3070f3
Luiz Fernando <lfaires@gmail.com>
2021-06-09 23:39:42 -0300

    metodo get pra obter os comments de cada post

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import fs from 'fs'
 const app = express()
 app.use(express.json());
 app.use(cors());
+const count = 0;
 
 const posts = [{
     id: 1,
@@ -14,14 +15,25 @@ const posts = [{
     contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     commentCount: 2
   }];
-console.log(posts)
+
+  const comments = [{
+    id: 1,
+    postId: 1,
+    author: 'João',
+    content: 'Muito bom esse post! Tá de parabéns'
+  }, {
+    id: 2,
+    postId: 1,
+    author: 'Maria',
+    content: 'Como faz pra dar palmas?'
+  }]
+
 app.get('/posts', (req,res) => {
     console.log("recebi um pedido para ter os posts")
     res.send(posts)
 })
 
 app.post('/posts', (req,res) => {
-    console.log("recebi um pedido para adicionar os posts")
     const contentPreview = 'Esta é a estrutura de um post esperado pelo front-end';
     const post = {id: posts.length +1,...req.body,contentPreview: contentPreview}
     posts.push(post)
@@ -36,6 +48,18 @@ app.get('/posts/:idDoPost', (req,res) => {
     res.send(post)
 })
 
+app.post('/posts/:idDoPost/comments', (req,res) =>{
+    const id = parseInt(req.params.idDoPost)
+    const comment = {id: count+1,...req.body}
+    comments.push(comment)
+    res.send(comment)
+})
+
+app.get('/posts/:idDoPost/comments', (req,res) =>{
+    const id = parseInt(req.params.idDoPost)
+    const comment = comments.filter( item => item.postId === id)
+    res.send(comment)
+})
 
 app.listen(4001, () =>{
     console.log("rodando o servidor!!!")
