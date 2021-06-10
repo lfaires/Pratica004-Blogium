60a5e194f3e33d9ec1ac487a380927ab21e335a4
Luiz Fernando <lfaires@gmail.com>
2021-06-09 22:40:27 -0300

    metodo get pra um post especifico

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-
+import fs from 'fs'
 
 const app = express()
 app.use(express.json());
@@ -10,11 +10,11 @@ const posts = [{
     id: 1,
     title: 'Hello World',
     coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-    contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
+    contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     commentCount: 2
   }];
-
+console.log(posts)
 app.get('/posts', (req,res) => {
     console.log("recebi um pedido para ter os posts")
     res.send(posts)
@@ -23,11 +23,20 @@ app.get('/posts', (req,res) => {
 app.post('/posts', (req,res) => {
     console.log("recebi um pedido para adicionar os posts")
     const contentPreview = 'Esta é a estrutura de um post esperado pelo front-end';
-    const post = {...req.body,id: posts.length +1,contentPreview: contentPreview}
+    const post = {id: posts.length +1,...req.body,contentPreview: contentPreview}
     posts.push(post)
+    res.send(post)
+    console.log(posts)
+})
+
+app.get('/posts/:idDoPost', (req,res) => {
+    const id = parseInt(req.params.idDoPost)
+    const post = posts.filter( item => item.id === id)[0]
     console.log(post)
     res.send(post)
 })
 
-console.log("rodando o servidor!!!")
-app.listen(4001)
+
+app.listen(4001, () =>{
+    console.log("rodando o servidor!!!")
+})
